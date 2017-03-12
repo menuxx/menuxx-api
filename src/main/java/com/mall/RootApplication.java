30862a27a388ce2f927d021c89865aa6449ae94a
yinchangsheng <ychangsheng@gmail.com>
2017-03-12 19:49:53 +0800

    remove target add to .gitignore

--- src/main/java/com/mall/RootApplication.java
+++ src/main/java/com/mall/RootApplication.java
@@ -3,20 +3,19 @@ package com.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableAsync
 public class RootApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("********************** RootApplication start 111 ************************");
+        System.out.println("********************** RootApplication start ************************");
         final ApplicationContext applicationContext = SpringApplication.run(RootApplication.class, args);
-        System.out.println("********************** RootApplication end 222 ************************");
+        System.out.println("********************** RootApplication end ************************");
     }
 
 }
